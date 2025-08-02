f221a80273d0955bfc8a3f941adc9d52c51741f1
Hariom Suman <635494hs@gmail.com>
2025-08-02 15:16:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from app import app, socketio
 import routes  # noqa: F401
 
+# For gunicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # use PORT from Render environment
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    debug = os.environ.get("FLASK_ENV") == "development"
+    socketio.run(app, host="0.0.0.0", port=port, debug=debug, allow_unsafe_werkzeug=True)
